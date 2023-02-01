20d71c0ae2982c3959b806df75c626bad0effd29
KVplayer <92183309+KVplayer@users.noreply.github.com>
2023-02-01 23:18:21 +0530

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -59,7 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
+        await update.reply_sticker("CAACAgUAAxkBAAIDKWPapOJYWsqPwc_Yj18cz9MJGrV6AAI0BwACQhiwV-XJUpXy2WgkLgQ")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
